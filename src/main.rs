bc1cab685767e85792aa6a2d8f4e6811ab1af309
梁典典 <413153189@qq.com>
2025-01-10 16:16:42 +0800

    0.2.1

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 use duct::cmd;
-use std::{env, io};
+use futures::future;
+use std::{env, io, sync::Arc};
 use tera::{Context, Tera};
 use tokio::{
     fs::{create_dir, File, OpenOptions},
     io::AsyncWriteExt,
+    task,
 };
+use tracing::info;
+use tracing_subscriber::fmt;
+
+fn set_log_event() {
+    // 初始化 tracing 子系统
+    let subscriber = fmt::Subscriber::builder()
+        .with_max_level(tracing::Level::INFO)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+}
+
 fn get_path_env() -> String {
     env::var("PATH").unwrap()
 }
 //获取插件名
 fn get_plugin_name() -> String {
     let mut input = String::new();
-    println!("请输入合法dart插件名字(例:hello_dart,hi_ldd_plugin):");
+    info!("请输入合法dart插件名字(例:hello_dart,hi_ldd_plugin):");
     let _ = io::stdin().read_line(&mut input).expect("读取项目名失败");
     input = input.trim().to_string();
     input
@@ -19,7 +32,7 @@ fn get_plugin_name() -> String {
 
 //创建插件项目目录
 async fn run_flutter_plugin_create(plugin_name: &str) -> bool {
-    println!("插件名:{}", plugin_name);
+    info!("开始创建插件目录:{}", plugin_name);
     let result = cmd!(
         "flutter",
         "create",
@@ -30,19 +43,33 @@ async fn run_flutter_plugin_create(plugin_name: &str) -> bool {
     )
     .dir(env::current_dir().expect("获取目录失败"))
     .env("env", get_path_env())
+    .stdout_null()
     .run();
     result.is_ok()
 }
 
+///初始化git项目,并克隆cargokit项目
 async fn init_git_config(plugin_name: &str) {
+    info!("开始初始化(git):{plugin_name}");
     let mut p = env::current_dir().unwrap();
     p.push(plugin_name);
-    cmd!("git", "init").dir(p.clone()).run().unwrap();
-    cmd!("git", "add", "--all").dir(p.clone()).run().unwrap();
-    cmd!("git", "commit", "-m", "initial commit")
+    cmd!("git", "init")
         .dir(p.clone())
+        .stdout_null()
         .run()
         .unwrap();
+    cmd!("git", "add", "--all")
+        .dir(p.clone())
+        .stdout_null()
+        .run()
+        .unwrap();
+    cmd!("git", "commit", "-m", "initial commit")
+        .dir(p.clone())
+        .stdout_null()
+        .stderr_null()
+        .run()
+        .unwrap();
+    info!("开始下载cargokit...");
     cmd!(
         "git",
         "subtree",
@@ -54,11 +81,14 @@ async fn init_git_config(plugin_name: &str) {
         "--squash"
     )
     .dir(p.clone())
+    .stdout_null()
     .run()
     .unwrap();
+    info!("✅初始化git环境成功");
 }
 
 async fn add_rust_lib_project(plugin_name: &str) {
+    info!("开始初始化rust lib项目");
     let mut p = env::current_dir().unwrap();
     p.push(plugin_name);
     cmd!(
@@ -70,6 +100,7 @@ async fn add_rust_lib_project(plugin_name: &str) {
         format!("{plugin_name}")
     )
     .dir(p.clone())
+    .stdout_null()
     .run()
     .unwrap();
     p.push("rust");
@@ -80,17 +111,16 @@ async fn add_rust_lib_project(plugin_name: &str) {
         .write_all(txt.as_bytes())
         .await
         .expect("写入配置失败");
+    info!("✅创建rust包成功");
 }
 
 ///从github上加载
 async fn fetch_github_temp_file_string(file_name: &str) -> Result<String, reqwest::Error> {
-    let response = reqwest::get(
-        format!("https://raw.githubusercontent.com/mdddj/frb_plugin_tool/main/temp/{file_name}")
-            .as_str(),
-    )
-    .await?
-    .text()
-    .await?;
+    let url =
+        format!("https://raw.githubusercontent.com/mdddj/frb_plugin_tool/main/temp/{file_name}");
+    info!("开始从github下载模板:{url}");
+    let response = reqwest::get(url.as_str()).await?.text().await?;
+    info!("✅加载模板引擎文本成功 {file_name}");
     Ok(response)
 }
 
@@ -127,6 +157,7 @@ async fn add_frb_yaml_file(plugin_name: &str) {
     file.write_all(text.as_bytes())
         .await
         .expect("写入frb配置失败");
+    info!("✅写入flutter_rust_bridge.yaml成功");
 }
 
 ///添加macos脚本
@@ -143,6 +174,7 @@ async fn add_macos_script(plugin_name: &str) {
     file.write_all(temp.as_bytes())
         .await
         .expect(&format!("写入{file_name}配置失败"));
+    info!("✅添加macos脚本成功");
 }
 
 ///添加ios脚本
@@ -159,6 +191,7 @@ async fn add_ios_script(plugin_name: &str) {
     file.write_all(temp.as_bytes())
         .await
         .expect(&format!("写入{file_name}配置失败"));
+    info!("✅添加ios脚本成功");
 }
 
 ///添加windows脚本
@@ -174,6 +207,7 @@ async fn add_windows_script(plugin_name: &str) {
     file.write_all(temp.as_bytes())
         .await
         .expect(&"写入windows CMakeLists.txt配置失败".to_string());
+    info!("✅添加windows脚本成功")
 }
 
 ///添加linux脚本
@@ -189,6 +223,7 @@ async fn add_linux_script(plugin_name: &str) {
     file.write_all(temp.as_bytes())
         .await
         .expect(&"写入linux CMakeLists.txt配置失败".to_string());
+    info!("✅添加linux脚本成功")
 }
 
 ///添加android脚本
@@ -204,6 +239,7 @@ async fn add_android_script(plugin_name: &str) {
     file.write_all(temp.as_bytes())
         .await
         .expect(&"写入build.gradle配置失败".to_string());
+    info!("✅添加android脚本成功")
 }
 
 ///添加yaml依赖
@@ -218,6 +254,7 @@ async fn add_pubspec_script(plugin_name: &str) {
     file.write_all(temp.as_bytes())
         .await
         .expect(&"写入pubspec.yaml配置失败".to_string());
+    info!("✅添加yaml依赖成功");
 }
 
 ///添加示例rust目录和文件 /api/hello.rs
@@ -229,7 +266,7 @@ async fn add_test_rs_file(plugin_name: &str) {
     dir.push("api");
     create_dir(&dir)
         .await
-        .expect("创建rust api 目录失败.请手动创建");
+        .expect("创建rust/api目录失败.请手动创建");
     dir.push("mod.rs");
     let mut file = OpenOptions::new()
         .write(true)
@@ -254,7 +291,6 @@ async fn add_test_rs_file(plugin_name: &str) {
     pub fn hello(hello: &str) {
         println!("hello world!");
     }
-    
     "#
         .as_bytes(),
     )
@@ -270,22 +306,53 @@ async fn add_test_rs_file(plugin_name: &str) {
     file.write_all("pub mod api;".as_bytes())
         .await
         .expect("写入lib.rs失败");
+    info!("✅写入rust test api成功")
 }
 
 #[tokio::main]
 async fn main() {
-    let plugin_name = get_plugin_name();
+    set_log_event();
+    let plugin_name = Arc::new(get_plugin_name());
     let is_ok = run_flutter_plugin_create(&plugin_name).await;
     if is_ok {
-        init_git_config(&plugin_name).await;
-        add_rust_lib_project(&plugin_name).await;
-        add_frb_yaml_file(&plugin_name).await;
-        add_macos_script(&plugin_name).await;
-        add_ios_script(&plugin_name).await;
-        add_windows_script(&plugin_name).await;
-        add_linux_script(&plugin_name).await;
-        add_android_script(&plugin_name).await;
-        add_pubspec_script(&plugin_name).await;
-        add_test_rs_file(&plugin_name).await;
+        let name = Arc::clone(&plugin_name);
+
+        let git_task = task::spawn(async move { init_git_config(&name).await });
+        let _ = git_task.await;
+        // add_frb_yaml_file(&plugin_name).await;
+        // add_macos_script(&plugin_name).await;
+        // add_ios_script(&plugin_name).await;
+        // add_windows_script(&plugin_name).await;
+        // add_linux_script(&plugin_name).await;
+        // add_android_script(&plugin_name).await;
+        // add_pubspec_script(&plugin_name).await;
+        // add_test_rs_file(&plugin_name).await;
+        let yaml_name = Arc::clone(&plugin_name);
+        let add_rust_name = Arc::clone(&plugin_name);
+        let macos_name = Arc::clone(&plugin_name);
+        let ios_name = Arc::clone(&plugin_name);
+        let windows_name = Arc::clone(&plugin_name);
+        let linux_name = Arc::clone(&plugin_name);
+        let android_name = Arc::clone(&plugin_name);
+        let pubspc_name = Arc::clone(&plugin_name);
+        let test_name = Arc::clone(&plugin_name);
+
+        let tasks = vec![
+            task::spawn(async move { add_rust_lib_project(&add_rust_name).await }),
+            task::spawn(async move { add_frb_yaml_file(&yaml_name).await }),
+            task::spawn(async move { add_macos_script(&macos_name).await }),
+            task::spawn(async move { add_ios_script(&ios_name).await }),
+            task::spawn(async move { add_windows_script(&windows_name).await }),
+            task::spawn(async move { add_linux_script(&linux_name).await }),
+            task::spawn(async move { add_android_script(&android_name).await }),
+            task::spawn(async move { add_pubspec_script(&pubspc_name).await }),
+        ];
+        future::join_all(tasks).await;
+        info!("✅项目创建成功,开始写入test文件");
+        let add_file_task = vec![task::spawn(
+            async move { add_test_rs_file(&test_name).await },
+        )];
+        future::join_all(add_file_task).await;
+
     }
 }
